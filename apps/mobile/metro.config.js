8905057468e8951a0d330cc88f68ec0c194ef450
Roman Dubinin <romanonthego@gmail.com>
2023-02-22 17:58:47 +0700

    IOS WORKING

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,7 +1,22 @@
+// @ts-check
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+// #1 - Watch all files in the monorepo
+config.watchFolders = [workspaceRoot];
+// #3 - Force resolving nested modules to the folders below
+config.resolver.disableHierarchicalLookup = true;
+// #2 - Try resolving with project modules first, then workspace modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
 
 config.watcher = config.watcher || {};
 
@@ -12,13 +27,13 @@ config.watcher.watchman = config.watcher.watchman || {};
 // Add support for react-native-svg-transformer
 config.transformer = {
   ...config.transformer,
-  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
 };
 
 config.resolver = {
   ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
-  sourceExts: [...config.resolver.sourceExts, "svg"],
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
 };
 
 module.exports = config;
